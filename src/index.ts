0865acfca00e478ace4f3ff7952e1a3d66878093
Rchitects <johnnyfikcock@gmail.com>
2025-12-17 12:02:43 +0100

    add: rateLimiter stage (not implemented) #5

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export { PipePress } from "./core/pipepress";
 export * from "./core/types";
 export { Router } from "./core/router";
 export { basicHTTPLogger } from "./stages/basicHTTPLogger";
+export { rateLimiter } from "./stages/rateLimiter";
---
+++ src/stages/rateLimiter.ts
@@ -0,0 +1,11 @@
+/*** imports ***/
+import { PipeStage } from "../core/types"
+
+/*** stage-handler ***/
+export const rateLimiter = (): PipeStage<void> => {
+    return {
+        handler: async (ctx) => {
+
+        }
+    }
+}
