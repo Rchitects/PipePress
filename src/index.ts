a49c143a58b465792b7f5eb8574ddf25ba26c1ba
Rchitects <johnnyfikcock@gmail.com>
2026-01-14 14:27:54 +0100

    add: export basic errors

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@
 export { PipePress } from "./core/pipepress";
 export * from "./core/types";
 export { Router } from "./core/router";
+export * from "./core/error";
 export { basicHTTPLogger } from "./stages/basicHTTPLogger";
 export { rateLimiter } from "./stages/rateLimiter";
